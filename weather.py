a11e6c326760dc1fd67998e3daefa69b9a38e128
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-29 11:08:22 +0200

    Update weather.py
    API added

--- weather.py
+++ weather.py
@@ -1,3 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 import requests
+
+def get_weather(city, api_key):
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    complete_url = f"{base_url}q={city}&appid={api_key}&units=metric"
+    
+    response = requests.get(complete_url)
