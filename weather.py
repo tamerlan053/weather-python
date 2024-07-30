5115553afbe472a5a11318d0c43cfeb622484cc5
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-07-30 13:43:16 +0200

    Update weather.py
    weather report added

--- weather.py
+++ weather.py
@@ -7,13 +7,19 @@ def get_weather(city, api_key):
     complete_url = f"{base_url}q={city}&appid={api_key}&units=metric"
     
     response = requests.get(complete_url)
-
+    
     if response.status_code == 200:
         data = response.json()
         main = data['main']
         weather = data['weather'][0]
-
+        
         temperature = main['temp']
         pressure = main['pressure']
         humidity = main['humidity']
         description = weather['description']
+        
+        weather_report = (f"City: {city}\n"
+                          f"Temperature: {temperature}°C\n"
+                          f"Pressure: {pressure} hPa\n"
+                          f"Humidity: {humidity}%\n"
+                          f"Weather: {description}")
